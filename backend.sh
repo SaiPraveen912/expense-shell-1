d643af58862332bd7194e10d917f9b91ba2def32
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-25 11:59:07 +0530

    sql-backend-frontend-common

--- backend.sh
+++ backend.sh
@@ -1,36 +1,11 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S) # 2024-04-21-07-00-14 -> Which time this is getting executed
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # $0 -> to get the script name
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m" # red color symbol
-G="\e[32m" # green color symbol
-Y="\e[33m"
-N="\e[0m"  # normal color symbol
+source ./common.sh
+
+check_root
+
 echo "Please enter DB password:"
 read -s mysql_root_password
-
-
-VALIDATE(){
-    if [ $1 -ne 0 ] # $1 have exit status of cmd : dnf install mysql -y
-    then
-        echo -e "$2...$R FAILURE $N" # -e for enabling colors $R for red $N for normal
-        exit 1 # if FAILURE then only exit ortherwise no need to exit
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi    
-}
-
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with super user"
-    exit 1 # manually exit if error comes
-else
-    echo "You are super user"
-fi
-
 
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling default nodejs"
