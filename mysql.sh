7e7b6586506c755948f46065a6a7913d0d268442
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-25 12:39:51 +0530

    shell-set-mysql

--- mysql.sh
+++ mysql.sh
@@ -7,7 +7,7 @@ check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-serddver -y &>>$LOGFILE
+dnf install mysql-server -y &>>$LOGFILE
 #VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
@@ -23,7 +23,7 @@ systemctl start mysqld &>>$LOGFILE
 mysql -h db.daws-78s.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    mysql_secure_infggxfgdstallation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     #VALIDATE $? "MySQL root password setup"
 else
     echo -e "MySQL root password is already setup...$Y SKIPPING $N"
